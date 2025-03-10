784a4e88b6bc4e424b2d95284ecd61aeddd31401
huxw <huxw@mail.jj.cn>
2025-03-10 21:01:41 +0800

    1. 调整参数映射模式

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+import { existsSync } from 'node:fs'
+import path from 'node:path'
 import process from 'node:process'
 import { defineExtension } from 'reactive-vscode'
 import * as vscode from 'vscode'
 import openWebview from './commands/openWebview'
+import { getCurrentWorkspaceFolder } from './utils/vscode'
 
 console.log('process.env.NODE_ENV:', process.env.NODE_ENV)
 
@@ -9,6 +12,15 @@ const { activate, deactivate } = defineExtension((context) => {
   const openWebviewCommand = vscode.commands.registerCommand(
     'extension.dirge.open-webview',
     (uri: vscode.Uri) => {
+      const workspaceFolder = getCurrentWorkspaceFolder()
+      if (!workspaceFolder)
+        return vscode.window.showErrorMessage('未打开任何工作区')
+
+      const configPath = path.join(workspaceFolder, 'lanhu.config.json')
+      if (!existsSync(configPath)) {
+        return vscode.window.showErrorMessage('当前工作区下未配置lanhu.config.json')
+      }
+
       openWebview(context, uri)
     },
   )
--- src/utils/messagener/util.ts
+++ src/utils/messagener/util.ts
@@ -1,27 +1,18 @@
-import type { BinDownloadOptions } from 'lanhu-image-downloader'
+import type { BinDownloadOptions } from 'lanhu-image-downloader/dist/downloadBy.type'
 
 const defaultMapFun = (val: any) => `${val}`.trim()
 
-const envMapConfig: Record<keyof BinDownloadOptions, (val: any) => string> = {
-  configFilePath: defaultMapFun,
-  sectorName: defaultMapFun,
-  type: defaultMapFun,
-  targetFolder: defaultMapFun,
-  downloadScale: defaultMapFun,
-  resizeScale: defaultMapFun,
+const envMapConfig: Partial<Record<keyof BinDownloadOptions, (val: any) => string>> = {
   enableTranslation(val: string) {
     return `${+val || 0}`
   },
-  teamId: defaultMapFun,
-  projectId: defaultMapFun,
-  imageId: defaultMapFun,
 }
 
 export function optionsToEnv(options: BinDownloadOptions) {
   const result: Record<string, any> = {}
 
   for (const [key, value] of Object.entries(options)) {
-    const transform = envMapConfig[key as keyof BinDownloadOptions]
+    const transform = envMapConfig[key as keyof BinDownloadOptions] || defaultMapFun
     const tValue = transform(value)
     result[key] = tValue
   }
